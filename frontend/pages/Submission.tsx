427532bff67da23367cddb406f56156d8c50fa33
kayishau <ulyssekayisha@gmail.com>
2023-09-23 19:29:36 -0400

    Submission page boiler

--- frontend/pages/Submission.tsx
+++ frontend/pages/Submission.tsx
@@ -1,4 +1,5 @@
 import styles from "./Submission.module.css"
+import { FiUpload } from 'react-icons/fi';
 
 export default function Submission() {
   return (
@@ -16,7 +17,21 @@ export default function Submission() {
     </form>
     </div>
     <div className={styles.upload_box}>
-
+    <div>
+      <FiUpload className={styles.upload_icon}/>
+    </div>
+      <form action="/action_page.php" 
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}>
+          <input
+           style={{
+            margin: "15px"
+          }}
+           type="file" id="myFile" name="filename"/>
+          <input type="submit" />
+      </form>
     </div>
     </div>
 
