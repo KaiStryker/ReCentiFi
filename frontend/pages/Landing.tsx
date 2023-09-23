fd4c63fff5b19ab73cf37db629586efb5a3b5c35
kayishau <ulyssekayisha@gmail.com>
2023-09-23 16:17:06 -0400

    'Landing boiler done, needs Worldcoin intergration

--- frontend/pages/Landing.tsx
+++ frontend/pages/Landing.tsx
@@ -1,5 +1,8 @@
 import styles from "./Landing.module.css"
 // import styles from "./footer.module.css";
+import Link from "next/link";
+
+
 
 export default function Landing() {
   return (
@@ -12,11 +15,16 @@ export default function Landing() {
           <div>
 
         {/* TODO:
-         Insert Worldcpin Verification here */}
+         1. Insert Worldcpin Verification here 
+         2. Fix Line Breaks*/}
+            <p>To submit a new pickup: {'\n'} 
+            1. Verify your proof of humanity with Worldcoin {'\n'} 
+            2. Click on "Submit a new pickup here" below
+            </p>
+            <h4> Worldcoin Verifcation</h4>
             <button>
-              Submit a new pickup
+            <Link href="/Submission">Submit a new pickup here.</Link>
             </button>
-            <p> Worldcoin Verifcation</p>
           </div>
         </div>
       </div>
