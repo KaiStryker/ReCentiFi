2bd9116f468e1fe8ca5bd63d6a36b0f594f5d25a
kayishau <ulyssekayisha@gmail.com>
2023-09-23 17:56:52 -0400

    start submission boiler

--- frontend/pages/Submission.tsx
+++ frontend/pages/Submission.tsx
@@ -1,14 +1,25 @@
-
+import styles from "./Submission.module.css"
 
 export default function Submission() {
   return (
   <>
-  <div>
-    <h1 style={{
-      color: "red"
-    }}>
-        Submission page
+  <div className={styles.subumission_main_container}>
+    <div>
+    <h1>
+       Upload your Submission to Filecoin
     </h1>
+    </div>
+    <div className={styles.submission_sub_container}>
+    <div className={styles.form}>
+    <form>
+
+    </form>
+    </div>
+    <div className={styles.upload_box}>
+
+    </div>
+    </div>
+
   </div>
   </>
   );
