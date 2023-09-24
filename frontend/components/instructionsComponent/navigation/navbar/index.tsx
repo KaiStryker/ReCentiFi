8c919eb0434e6199b4bd7a80012790360879dc06
kayishau <ulyssekayisha@gmail.com>
2023-09-24 05:13:53 -0400

    Landing basic styling

--- frontend/components/instructionsComponent/navigation/navbar/index.tsx
+++ frontend/components/instructionsComponent/navigation/navbar/index.tsx
@@ -6,10 +6,10 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+      {/* <a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
         <p>create-web3-dapp</p>
-      </a>
-      <ConnectKitButton />
+      </a> */}
+      {/* <ConnectKitButton /> */}
     </nav>
   );
 }
--- frontend/pages/Landing.tsx
+++ frontend/pages/Landing.tsx
@@ -8,28 +8,33 @@ export default function Landing() {
   return (
     <>
       <div className={styles.landing}>
+        <div className={styles.landing_sub_container1}>
         <div>
+          <h1 className={styles.h1}>
+            <span  className={styles.span}>Re</span>Centi<span className={styles.span}>Fi</span></h1>
+        </div>
           <div>
-            <h1>How many pieces of Litter did you pickup today?</h1>
+            <h2 className={styles.h2}>How many pieces of Litter did you pickup today?</h2>
           </div>
           <div>
 
         {/* TODO:
          1. Insert Worldcpin Verification here 
          2. Fix Line Breaks*/}
-            <p>To submit a new pickup: {'\n'} 
-            1. Verify your proof of humanity with Worldcoin {'\n'} 
-            2. Click on "Submit a new pickup here" below
-            </p>
-            <h4> Worldcoin Verifcation</h4>
-            <button>
-            <Link href="/Submission">Submit a new pickup here.</Link>
+            <p className={styles.p}>To submit a new pickup:</p>
+            <p className={styles.p}>1. Verify your proof of humanity with Worldcoin </p>
+            <p className={styles.p}>2. Click on "Submit a new pickup here" below</p>
+            <h4 className={styles.h2}> Worldcoin Verifcation</h4>
+            <button className={styles.button}>
+            <Link className={styles.link_text} href="/Submission">Submit a new pickup here.</Link>
             </button>
           </div>
         </div>
+        <div className={styles.landing_sub_container2}>
+        </div>
       </div>
       <div>
-        
+     
       </div>
     </>
   );
